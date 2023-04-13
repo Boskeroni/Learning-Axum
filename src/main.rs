9038f9a355f283147c721216119663ade6ffd4ab
chonkyboy <boskeroni@gmail.com>
2023-04-13 16:16:09 +0100

    understand how it works now. Want to use a js framework though

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use axum::{routing::get, Router, Server};
+use axum::{routing::get, Router, Server, Json, response::IntoResponse};
 
 #[tokio::main]
 async fn main() {
     let router = Router::new()
-        .route("/", get(root))
-        .route("/other", get(other));
+        .route("/", get(root));
 
     let server = Server::bind(&"0.0.0.0:3000".parse().unwrap()).serve(router.into_make_service());
     let port = server.local_addr();
@@ -13,5 +12,15 @@ async fn main() {
     server.await.unwrap();
 }
 
-async fn root() {}
-async fn other() {}
+#[axum::debug_handler]
+async fn root() -> impl IntoResponse {
+    let mut all_files = Vec::new();
+    let mut path = tokio::fs::read_dir("./").await.unwrap();
+
+    while let Ok(Some(e)) = path.next_entry().await {
+        let name = e.file_name().to_str().unwrap().to_string();
+        all_files.push(name);
+    }
+
+    Json(all_files)
+}
